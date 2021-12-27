be80ff2b955f457461d806603ee5ad6173ef0890
LuthfiRobit <luthfirobit@gmail.com>
2021-12-27 10:59:38 +0700

    create model penugasan driver

---
+++ app/Models/PenugasanDriver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PenugasanDriver extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'tb_penugasan_driver';
+    protected $primaryKey = 'id_do';
+
+    protected $fillable = [
+        'id_service_order',
+        'id_driver',
+        'id_kendaraan',
+        'id_petugas',
+        'tgl_penugasan',
+        'jam_berangkat',
+        'kembali',
+        'tgl_acc',
+        'status_penugasan'
+    ];
+}
