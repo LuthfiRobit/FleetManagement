a461294540a5da37bdb85f73d7d228d3fbe3ca6e
LuthfiRobit <luthfirobit@gmail.com>
2021-12-29 20:44:53 +0700

    add route latest id order

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::group(
     ['middleware' => 'api', 'prefix' => 'service'],
     function () {
         Route::post('create', [ApiServiceOrderController::class, 'createSo']);
+        Route::get('latest', [ApiServiceOrderController::class, 'getLastIdDo']);
         Route::get('list', [ApiServiceOrderController::class, 'getDo']);
         Route::get('check/list/transport', [ApiServiceOrderController::class, 'listTransport']);
         Route::get('check/transport', [ApiServiceOrderController::class, 'checkTransport']);
