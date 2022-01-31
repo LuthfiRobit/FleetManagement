3daa5189e0d6c7e3ea6da4a8f7cfbdd7fdd1482e
LuthfiRobit <luthfirobit@gmail.com>
2022-02-01 01:45:53 +0700

    make jenis alokasi view

---
+++ resources/views/dashboard/pages/jenisAlokasi/index.blade.php
@@ -0,0 +1,303 @@
+@extends('layouts.backend.main')
+
+@section('title','Jenis Alokasi')
+@section('style-on-this-page-only')
+<link href="{{url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet"
+    type="text/css" />
+@endsection
+
+@section('content')
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Post-->
+    <div class="post d-flex flex-column-fluid" id="kt_post">
+        <!--begin::Container-->
+        <div id="kt_content_container" class="container-xxl">
+            <div class="card mb-5 mb-xl-8">
+                <!--begin::Header-->
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bolder fs-3 mb-1">Data Jenis Alokasi</span>
+                        <span class="text-muted mt-1 fw-bold fs-7">Lebih dari 2
+                            Jenis Alokasi</span>
+                    </h3>
+                    <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover"
+                        title="" data-bs-original-title="Tekan untuk menambah merk kendaraan">
+                        <a href="#" class="btn btn-sm btn-light btn-active-primary" data-bs-toggle="modal"
+                            data-bs-target="#kt_modal_new_merk">
+                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                            <span class="svg-icon svg-icon-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1"
+                                        transform="rotate(-90 11.364 20.364)" fill="black"></rect>
+                                    <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black"></rect>
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->Tambah Jenis Alokasi
+                        </a>
+                    </div>
+                </div>
+                <!--end::Header-->
+                <!--begin::Body-->
+                <div class="card-body py-3">
+                    <!--begin::Table container-->
+                    <table id="kt_datatable_example_5"
+                        class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4 display responsive nowrap">
+                        <thead>
+                            <tr class="fw-bolder fs-6 text-gray-800 px-7">
+                                <th class="w-25px">
+                                    <div class="form-check form-check-sm form-check-custom form-check-solid">
+                                        <input class="form-check-input" type="checkbox" value="1" data-kt-check="true"
+                                            data-kt-check-target=".widget-9-check">
+                                    </div>
+                                </th>
+                                <th>No</th>
+                                <th>Nama Jenis Alokasi</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($jenis_alokasi as $index => $ja)
+                            <tr>
+                                <td>
+                                    <div class="form-check form-check-sm form-check-custom form-check-solid">
+                                        <input class="form-check-input widget-9-check" type="checkbox" value="1">
+                                    </div>
+                                </td>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $ja->nama_alokasi }}</td>
+                                <td>
+                                    @if ($ja->status == 'y')
+                                    <span class="badge badge-light-primary">Aktif</span>
+                                    @else
+                                    <span class="badge badge-light-danger">Non Aktif</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    <div class="d-flex justify-content-end flex-shrink-0">
+                                        <a href="{{ route('dashboard.kendaraan.jenis_alokasi.edit',$ja->id_jenis_alokasi)}}"
+                                            class=" btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
+                                            <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
+                                            <span class="svg-icon svg-icon-2">
+                                                <i class="bi bi-pencil-square fs-6"></i>
+                                            </span>
+                                            <!--end::Svg Icon-->
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                    <!--end::Table container-->
+                </div>
+                <!--begin::Body-->
+            </div>
+        </div>
+        <!--end::Container-->
+    </div>
+
+    <!--begin::Modal - New Target-->
+    <div class="modal fade" id="kt_modal_new_merk" tabindex="-1" aria-hidden="true">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered mw-650px">
+            <!--begin::Modal content-->
+            <div class="modal-content rounded">
+                <!--begin::Modal header-->
+                <div class="modal-header pb-0 border-0 justify-content-end">
+                    <!--begin::Close-->
+                    <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+                        <span class="svg-icon svg-icon-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                    transform="rotate(-45 6 17.3137)" fill="black" />
+                                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                    fill="black" />
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Close-->
+                </div>
+                <!--begin::Modal header-->
+                <!--begin::Modal body-->
+                <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
+                    <!--begin:Form-->
+                    <form id="kt_modal_new_target_form" class="form"
+                        action="{{route('dashboard.kendaraan.jenis_alokasi.store')}}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        <!--begin::Heading-->
+                        <div class="mb-13 text-center">
+                            <!--begin::Title-->
+                            <h1 class="mb-3">Input Jenis Alokasi</h1>
+                            <!--end::Title-->
+                        </div>
+                        <!--end::Heading-->
+                        <!--begin::Input group-->
+                        <div class="d-flex flex-column mb-8 fv-row">
+                            <!--begin::Label-->
+                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                <span class="required">Nama Jenis Alokasi</span>
+                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                    title="Sesuaikan Dengan Data Perusahaan"></i>
+                            </label>
+                            <!--end::Label-->
+                            <input type="text" class="form-control form-control-solid"
+                                placeholder="Masukkan Nama Jenis Alokasi" name="nama_alokasi" />
+                        </div>
+                        <div class="d-flex flex-column mb-8 fv-row">
+                            <!--begin::Label-->
+                            {{-- <div class="col-md-6 fv-row"> --}}
+                                <label class="required fs-6 fw-bold mb-2">Status (Aktif/Non Aktif)</label>
+                                <select class="form-select form-select-solid" data-control="select2"
+                                    data-hide-search="true" data-placeholder="Pilih Status" name="status">
+                                    <option value="">Pilih Status</option>
+                                    <option value="y">Aktif</option>
+                                    <option value="t">Non Aktif</option>
+                                </select>
+                                {{--
+                            </div> --}}
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Actions-->
+                        <div class="text-center">
+                            <button type="reset" id="kt_modal_new_target_cancel"
+                                class="btn btn-light me-3">Cancel</button>
+                            <button type="submit" id="kt_modal_new_target_submit" class="btn btn-primary">
+                                <span class="indicator-label">Submit</span>
+                                <span class="indicator-progress">Please wait...
+                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                            </button>
+                        </div>
+                        <!--end::Actions-->
+                    </form>
+                    <!--end:Form-->
+                </div>
+                <!--end::Modal body-->
+            </div>
+            <!--end::Modal content-->
+        </div>
+        <!--end::Modal dialog-->
+    </div>
+    <!--end::Modal - New Target-->
+    <!--end::Post-->
+</div>
+@endsection
+
+@push('scripts')
+<!--begin::Page Vendors Javascript(used by this page)-->
+<script src="{{ url('assets/backend/assets/plugins/custom/prismjs/prismjs.bundle.js') }}"></script>
+<script src="{{ url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+<!--end::Page Vendors Javascript-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="{{ url('assets/backend/assets/js/custom/documentation/documentation.js') }}"></script>
+<script src="{{ url('assets/backend/assets/js/custom/documentation/search.js') }}"></script>
+<script src="{{ url('assets/backend/assets/js/custom/documentation/general/datatables/advanced.js') }}"></script>
+<!--end::Page Custom Javascript-->
+<script text="text/javascipt">
+    "use strict";
+var KTModalNewTarget = function () {
+    var t, e, n, a, o, i;
+    return {
+        init: function () {
+            (i = document.querySelector("#kt_modal_new_merk")) && (o = new bootstrap.Modal(i),
+                a = document.querySelector("#kt_modal_new_target_form"),
+                t = document.getElementById("kt_modal_new_target_submit"),
+                e = document.getElementById("kt_modal_new_target_cancel"),
+                $(a.querySelector('[name="nama_alokasi"]')).on("change", (function () {
+                    n.revalidateField("nama_jenis_alokasi")
+                })), n = FormValidation.formValidation(a, {
+                    fields: {
+                        nama_alokasi: {
+                            validators: {
+                                notEmpty: {
+                                    message: "Nama Jenis Alokasi Harus Diisi"
+                                },
+                                stringLength: {
+                                    // options: {
+                                    max: 50,
+                                    message: "Nama Jenis Alokasi 50 Karakter"
+                                    // }
+                                }
+                            }
+                        },
+                        status: {
+                            validators: {
+                                notEmpty: {
+                                    message: "Status Jenis Alokasi Harus Diisi"
+                                }
+                            }
+                        }
+                    },
+                    plugins: {
+                        trigger: new FormValidation.plugins.Trigger,
+                        bootstrap: new FormValidation.plugins.Bootstrap5({
+                            rowSelector: ".fv-row",
+                            eleInvalidClass: "",
+                            eleValidClass: ""
+                        })
+                    }
+                }),
+                t.addEventListener("click", (function (e) {
+                    e.preventDefault(), n && n.validate().then((function (e) {
+                        console.log("validated!"), "Valid" == e ? (t.setAttribute("data-kt-indicator", "on"), t.disabled = !0, setTimeout((function () {
+                            t.removeAttribute("data-kt-indicator"), t.disabled = !1, Swal.fire({
+                                text: "Formulir telah berhasil dikirim!",
+                                icon: "success",
+                                buttonsStyling: !1,
+                                confirmButtonText: "Ok, got it!",
+                                customClass: {
+                                    confirmButton: "btn btn-primary"
+                                }
+                            }).then((function (t) {
+                                a.submit()
+                                t.isConfirmed && o.hide()
+                            }))
+                        }), 2e3)) : Swal.fire({
+                            text: "Maaf, sepertinya ada beberapa kesalahan yang terdeteksi, silakan coba lagi.",
+                            icon: "error",
+                            buttonsStyling: !1,
+                            confirmButtonText: "Ok, got it!",
+                            customClass: {
+                                confirmButton: "btn btn-primary"
+                            }
+                        })
+                    }))
+                })),
+                e.addEventListener("click", (function (t) {
+                    t.preventDefault(), Swal.fire({
+                        text: "Apakah Anda yakin ingin membatalkan?",
+                        icon: "warning",
+                        showCancelButton: !0,
+                        buttonsStyling: !1,
+                        confirmButtonText: "Yes, cancel it!",
+                        cancelButtonText: "No, return",
+                        customClass: {
+                            confirmButton: "btn btn-primary",
+                            cancelButton: "btn btn-active-light"
+                        }
+                    }).then((function (t) {
+                        t.value ? (a.reset(), o.hide()) : "cancel" === t.dismiss && Swal.fire({
+                            text: "Formulir Anda belum dibatalkan!.",
+                            icon: "error",
+                            buttonsStyling: !1,
+                            confirmButtonText: "Ok, got it!",
+                            customClass: {
+                                confirmButton: "btn btn-primary"
+                            }
+                        })
+                    }))
+                })))
+        }
+    }
+}();
+KTUtil.onDOMContentLoaded((function () {
+    KTModalNewTarget.init()
+}));
+</script>
+@endpush
