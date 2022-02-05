c32be60b682bffbed15a58f4e20d51ea694cf1df
LuthfiRobit <luthfirobit@gmail.com>
2022-02-05 15:02:03 +0700

    add function penugasanTerbaru,detailPenugasan,terimaPenugasan,listPenugasan

--- app/Http/Controllers/Api/ApiPenugasanController.php
+++ app/Http/Controllers/Api/ApiPenugasanController.php
@@ -6,11 +6,175 @@ use App\Http\Controllers\Controller;
 use App\Models\PenugasanBatal;
 use App\Models\PenugasanDriver;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApiPenugasanController extends Controller
 {
-    public function index()
+    public function penugasanTerbaru(Request $request)
     {
+        $id_dr = $request->query('id_driver');
+        $latestDo = DB::table('tb_penugasan_driver')
+            ->select(
+                'tb_penugasan_driver.id_do',
+                'tb_penugasan_driver.id_driver',
+                'tb_petugas.nama_lengkap as nama_petugas',
+                'tb_petugas.no_tlp',
+                'tb_driver.nama_driver',
+                'tb_kendaraan.nama_kendaraan',
+                'tb_kendaraan.no_polisi',
+                'tb_penugasan_driver.tgl_penugasan',
+                'tb_penugasan_driver.jam_berangkat',
+                'tb_penugasan_driver.tmp_penjemputan as jemput',
+                'tb_penugasan_driver.tmp_tujuan as tujuan',
+                'tb_penugasan_driver.kembali',
+                'tb_penugasan_driver.tgl_acc',
+                'tb_penugasan_driver.status_penugasan'
+            )
+            ->join('tb_petugas', 'tb_petugas.id_petugas', '=', 'tb_penugasan_driver.id_petugas')
+            ->join('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->join('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->orderByDesc('id_do')
+            ->where([['tb_penugasan_driver.id_driver', $id_dr], ['status_penugasan', null]])
+            ->get();
+
+        if ($latestDo->count() > 0) {
+            return response()->json(
+                [
+                    'status'        => 'sukses',
+                    'terbaru'     => $latestDo
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'status'        => 'gagal',
+                    'terbaru'     => 'kosong'
+                ]
+            );
+        }
+    }
+
+    public function detailPenugasan(Request $request)
+    {
+        $id_do = $request->query('id_do');
+        $id_dr = $request->query('id_driver');
+        $detail = DB::table('tb_penugasan_driver')
+            ->select(
+                'tb_penugasan_driver.id_do',
+                'tb_penugasan_driver.id_driver',
+                'tb_petugas.nama_lengkap as nama_petugas',
+                'tb_petugas.no_tlp',
+                'tb_driver.nama_driver',
+                'tb_kendaraan.nama_kendaraan',
+                'tb_kendaraan.no_polisi',
+                'tb_penugasan_driver.tgl_penugasan',
+                'tb_penugasan_driver.jam_berangkat',
+                'tb_penugasan_driver.tmp_penjemputan as jemput',
+                'tb_penugasan_driver.tmp_tujuan as tujuan',
+                'tb_penugasan_driver.kembali',
+                'tb_penugasan_driver.tgl_acc',
+                'tb_penugasan_driver.status_penugasan'
+            )
+            ->join('tb_petugas', 'tb_petugas.id_petugas', '=', 'tb_penugasan_driver.id_petugas')
+            ->join('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->join('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->where([['tb_penugasan_driver.id_do', $id_do], ['tb_penugasan_driver.id_driver', $id_dr]])
+            ->first();
+
+        if ($detail) {
+            return response()->json(
+                [
+                    'status'        => 'sukses',
+                    'detail'     => $detail
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'status'        => 'gagal',
+                    'detail'     => 'kosong'
+                ]
+            );
+        }
+    }
+
+    public function terimaPenugasan(Request $request)
+    {
+        $id_dr = $request->id_driver;
+        $id_do = $request->id_do;
+        $acceptDo = PenugasanDriver::where([['id_do', $id_do], ['id_driver', $id_dr]])->first();
+        if ($acceptDo == true) {
+            $acceptDo->update(['status_penugasan' => 't']);
+            return response()->json(
+                [
+                    'status'        => 'sukses',
+                    'status_penugasan' => $acceptDo->status_penugasan
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'status'        => 'gagal'
+                ]
+            );
+        }
+    }
+
+    public function listPenugasan(Request $request)
+    {
+        $id_dr = $request->query('id_driver');
+        $tab = $request->query('tab');
+        $listDo = DB::table('tb_penugasan_driver')
+            ->select(
+                'tb_penugasan_driver.id_do',
+                'tb_penugasan_driver.id_driver',
+                'tb_petugas.nama_lengkap as nama_petugas',
+                'tb_petugas.no_tlp',
+                'tb_driver.nama_driver',
+                'tb_kendaraan.nama_kendaraan',
+                'tb_kendaraan.no_polisi',
+                'tb_penugasan_driver.tgl_penugasan',
+                'tb_penugasan_driver.jam_berangkat',
+                'tb_penugasan_driver.tmp_penjemputan as jemput',
+                'tb_penugasan_driver.tmp_tujuan as tujuan',
+                'tb_penugasan_driver.kembali',
+                'tb_penugasan_driver.tgl_acc',
+                'tb_penugasan_driver.status_penugasan'
+            )
+            ->join('tb_petugas', 'tb_petugas.id_petugas', '=', 'tb_penugasan_driver.id_petugas')
+            ->join('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->join('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->orderByDesc('id_do')
+            ->when($tab == 't', function ($status) use ($tab) {
+                $status->where('status_penugasan', 't');
+            })
+            ->when($tab == 'p', function ($status) use ($tab) {
+                $status->where('status_penugasan', 'p');
+            })
+            ->when($tab == 's', function ($status) use ($tab) {
+                $status->where('status_penugasan', 's');
+            })
+            ->when($tab == 'c', function ($status) use ($tab) {
+                $status->where('status_penugasan', 'c');
+            })
+            ->where([['tb_penugasan_driver.id_driver', $id_dr], ['status_penugasan', '!=', null]])
+            ->get();
+
+        if ($listDo->count() > 0) {
+            return response()->json(
+                [
+                    'status'        => 'sukses',
+                    'list_penugasan'     => $listDo
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'status'        => 'sukses',
+                    'list_penugasan'     => 'kosong'
+                ]
+            );
+        }
     }
 
     public function batalPenugasan(Request $request)
