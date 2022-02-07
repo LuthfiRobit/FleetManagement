3925909daef55de977f80029ee206cb0ef428cdf
LuthfiRobit <luthfirobit@gmail.com>
2022-02-07 23:09:24 +0700

    fixing detail

--- resources/views/dashboard/main/checking/detail.blade.php
+++ resources/views/dashboard/main/checking/detail.blade.php
@@ -121,10 +121,17 @@
                                     @else
                                     <span class="badge badge-light-warning">Pengecekan Kecelakaan</span>
                                     @endif
+                                    |
+                                    @if($pengecekan->status_kendaraan == 'r')
+                                    <span class="badge badge-light-success">Tersedia</span>
+                                    @else
+                                    <span class="badge badge-light-warning">Tidak Tersedia</span>
+                                    @endif
                                 </div>
                             </div>
                             <!--end::Card title-->
                             <!--begin::Card toolbar-->
+                            @if ($pengecekan->status_perbaikan == '')
                             <div class="card-toolbar">
                                 <!--begin::Add-->
                                 <button type="button" class="btn btn-light-primary btn-sm" data-kt-menu-trigger="click"
@@ -147,15 +154,31 @@
                                  <!--end::Menu item-->
                                  <!--begin::Menu item-->
                                  <div class=" menu-item px-3">
-                                            <a type="button" data-pengecekan="{{$pengecekan->id_pengecekan}}"
-                                                class="menu-link px-3" id="kt_modal_repair_reject">Tolak
-                                                Perbaikan</a>
+                                            <a href="#" class="menu-link px-3" data-bs-toggle="modal"
+                                                data-bs-target="#kt_modal_reject">Tolak Perbaikan</a>
                                     </div>
                                     <!--end::Menu item-->
                                 </div>
                                 <!--end::Menu-->
                                 <!--end::Add-->
                             </div>
+                            @elseif($pengecekan->status_perbaikan == 'n')
+                            <div
+                                class="notice d-flex bg-light-primary rounded border-primary border border-dashed mb-12 p-2">
+                                Dapat Beroprasi
+                            </div>
+                            @elseif($pengecekan->status_perbaikan == 't')
+                            <div
+                                class="notice d-flex bg-light-success rounded border-success border border-dashed mb-12 p-2">
+                                Sedang Diperbaiki
+                            </div>
+                            @elseif($pengecekan->status_perbaikan == 'tl')
+                            <div
+                                class="notice d-flex bg-light-warning rounded border-warning border border-dashed mb-12 p-2">
+                                Tidak Diperbaiki
+                            </div>
+                            @endif
+
                             <!--end::Card toolbar-->
                         </div>
                         <!--end::Card header-->
@@ -426,6 +449,84 @@
     </div>
     <!--end::Modal Repair-->
 
+    <!--begin::Modal - Reject-->
+    <div class="modal fade" id="kt_modal_reject" tabindex="-1" aria-hidden="true">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered mw-650px">
+            <!--begin::Modal content-->
+            <div class="modal-content rounded">
+                <!--begin::Modal header-->
+                <div class="modal-header pb-0 border-0 justify-content-end">
+                    <!--begin::Close-->
+                    <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+                        <span class="svg-icon svg-icon-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                    transform="rotate(-45 6 17.3137)" fill="black" />
+                                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                    fill="black" />
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Close-->
+                </div>
+                <!--begin::Modal header-->
+                <!--begin::Modal body-->
+                <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
+                    <!--begin:Form-->
+                    <form id="kt_modal_reject_form" class="form"
+                        action="{{route('repair.reject', $pengecekan->id_pengecekan)}}" method="POST">
+                        @csrf
+                        {{-- @method('PUT') --}}
+                        <!--begin::Heading-->
+                        <div class="mb-13 text-center">
+                            <!--begin::Title-->
+                            <h1 class="mb-3">Penolakan Perbaikan</h1>
+                            <!--end::Title-->
+                        </div>
+                        <!--end::Heading-->
+                        <!--begin::Input group-->
+                        <div class="d-flex flex-column mb-8 fv-row">
+                            <label class="required fs-6 fw-bold mb-2">NO. Pengecekan</label>
+                            <input type="text" class="form-control form-control-solid"
+                                value="VC_{{$pengecekan->id_pengecekan}}" disabled />
+                        </div>
+                        <div class="d-flex flex-column mb-8 fv-row">
+                            <!--begin::Label-->
+                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                <span class="required">Deskripsi Penolakan</span>
+                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                    title="Jelaskan Penolakan Perbaikan"></i>
+                            </label>
+                            <!--end::Label-->
+                            <textarea name="keterangan_penolakan" class="form-control form-control-solid"
+                                placeholder="Tuliskan Penolakan Perbaikan"></textarea>
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Actions-->
+                        <div class="text-center">
+                            <button type="reset" id="kt_modal_reject_cancel" class="btn btn-light me-3">Batal</button>
+                            <button type="submit" id="kt_modal_reject_submit" class="btn btn-primary">
+                                <span class="indicator-label">Kirim</span>
+                                <span class="indicator-progress">Mohon Tunggu Sebentar...
+                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                            </button>
+                        </div>
+                        <!--end::Actions-->
+                    </form>
+                    <!--end:Form-->
+                </div>
+                <!--end::Modal body-->
+            </div>
+            <!--end::Modal content-->
+        </div>
+        <!--end::Modal dialog-->
+    </div>
+    <!--end::Modal - Reject-->
+
 </div>
 @endsection
 
@@ -547,47 +648,80 @@
         KTModalUncheck.init()
     }));
 
-    var KTModalReject = {
-        init: function () {
-            document.getElementById("kt_modal_repair_reject").addEventListener("click", (t => {
-                t.preventDefault(), Swal.fire({
-                    text: "Apakah anda yakin menolak perbaikan?, laporan tidak bisa diganti",
-                    icon: "warning",
-                    showCancelButton: !0,
-                    buttonsStyling: !1,
-                    confirmButtonText: "Ya, saya yakin!",
-                    cancelButtonText: "Tidak, kembali",
-                    customClass: {
-                        confirmButton: "btn btn-primary",
-                        cancelButton: "btn btn-active-light"
-                    }
-                }).then((function (t) {
-                    t.value ?
-                    $.ajax({
-                        type: 'POST',
-                        url: '{{route("repair.reject")}}',
-                        data: {
-                            '_token': '{{csrf_token()}}',
-                            'id_pengecekan': $('#kt_modal_repair_reject').data('pengecekan')
+    var KTModalNewTarget = function () {
+        var t, e, n, a, o, i;
+        return {
+            init: function () {
+                (i = document.querySelector("#kt_modal_reject")) && (o = new bootstrap.Modal(i),
+                    a = document.querySelector("#kt_modal_reject_form"),
+                    t = document.getElementById("kt_modal_reject_submit"),
+                    e = document.getElementById("kt_modal_reject_cancel"),
+                    n = FormValidation.formValidation(a, {
+                        fields: {
+                            keterangan_penolakan: {
+                                validators: {
+                                    notEmpty: {
+                                        message: "Keterangan penolakan harus diisi"
+                                    },
+                                    stringLength: {
+                                        // options: {
+                                        max: 255,
+                                        message: "Maksimal 225 karakter."
+                                        // }
+                                    }
+                                }
+                            }
                         },
-                        datatype: 'JSON',
-                        success: function (response) {
-                            Swal.fire({
-                                text: "Anda telah menolak perbaikan!.",
-                                icon: "success",
+                        plugins: {
+                            trigger: new FormValidation.plugins.Trigger,
+                            bootstrap: new FormValidation.plugins.Bootstrap5({
+                                rowSelector: ".fv-row",
+                                eleInvalidClass: "",
+                                eleValidClass: ""
+                            })
+                        }
+                    }),
+                    t.addEventListener("click", (function (e) {
+                        e.preventDefault(), n && n.validate().then((function (e) {
+                            console.log("validated!"), "Valid" == e ? (t.setAttribute("data-kt-indicator", "on"), t.disabled = !0, setTimeout((function () {
+                                t.removeAttribute("data-kt-indicator"), t.disabled = !1, Swal.fire({
+                                    text: "Penolakan berhasil dikirim!",
+                                    icon: "success",
+                                    buttonsStyling: !1,
+                                    confirmButtonText: "Ok, saya mengerti!",
+                                    customClass: {
+                                        confirmButton: "btn btn-primary"
+                                    }
+                                }).then((function (t) {
+                                    a.submit()
+                                    t.isConfirmed && o.hide()
+                                }))
+                            }), 2e3)) : Swal.fire({
+                                text: "Maaf, ada beberapa error terdeteksi.",
+                                icon: "error",
                                 buttonsStyling: !1,
-                                confirmButtonText: "Ok, saya mengerti!",
+                                confirmButtonText: "Ok, got it!",
                                 customClass: {
                                     confirmButton: "btn btn-primary"
-                                },
-                            }),
-                            setTimeout(function(){// wait for 5 secs(2)
-                                location.reload(); // then reload the page.(3)
-                            }, 5000);
-                        },
-                        error: function (response) {
-                            Swal.fire({
-                                text: "Gagal tolak perbaikan!.",
+                                }
+                            })
+                        }))
+                    })),
+                    e.addEventListener("click", (function (t) {
+                        t.preventDefault(), Swal.fire({
+                            text: "Apakah anda yakin membatalkan penolakan?",
+                            icon: "warning",
+                            showCancelButton: !0,
+                            buttonsStyling: !1,
+                            confirmButtonText: "Iya, batalkan!",
+                            cancelButtonText: "Tidak, kembali!",
+                            customClass: {
+                                confirmButton: "btn btn-primary",
+                                cancelButton: "btn btn-active-light"
+                            }
+                        }).then((function (t) {
+                            t.value ? (a.reset(), o.hide()) : "cancel" === t.dismiss && Swal.fire({
+                                text: "Penolakan anda belum dibatalka!.",
                                 icon: "error",
                                 buttonsStyling: !1,
                                 confirmButtonText: "Ok, saya mengerti!",
@@ -595,24 +729,16 @@
                                     confirmButton: "btn btn-primary"
                                 }
                             })
-                        }
-                    })
-                    : "cancel" === t.dismiss && Swal.fire({
-                        text: "Tolak perbaikan dibatalkan, anda masih bisa mengganti laporan (diperbaiki/ditolak)!.",
-                        icon: "error",
-                        buttonsStyling: !1,
-                        confirmButtonText: "Ok, saya mengerti!",
-                        customClass: {
-                            confirmButton: "btn btn-primary"
-                        }
-                    })
-                }))
-            }))
+                        }))
+                    })))
+            }
         }
-    };
+    }();
     KTUtil.onDOMContentLoaded((function () {
-        KTModalReject.init()
+        KTModalNewTarget.init()
     }));
+
+
 
 </script>
 @endpush
