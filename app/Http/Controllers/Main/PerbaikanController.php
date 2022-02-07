02bb53da67d532fc7c2ef5a6f41f5f8b3a476348
LuthfiRobit <luthfirobit@gmail.com>
2022-02-07 20:39:57 +0700

    fixing perbaikan controller with new logic

--- app/Http/Controllers/Main/PerbaikanController.php
+++ app/Http/Controllers/Main/PerbaikanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\PengecekanKendaraanDetail;
 use App\Models\Perbaikan;
 use App\Models\PerbaikanPersetujuan;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -19,74 +20,91 @@ class PerbaikanController extends Controller
     {
 
         $id_pengecekan = $request->id_pengecekan;
-        $id_petugas = 1;
-        $status = $request->req_status;
+        $id_petugas = 4;
 
         $data_approval = [
             'no_wo' => $request->no_wo,
             'id_pengecekan' => $id_pengecekan,
             'id_petugas' => $id_petugas,
-            'tgl_persetujuan' => $request->tgl_persetujuan
+            'tgl_persetujuan' => $request->tgl_persetujuan,
+            'status' => 's'
         ];
 
-        if ($status == 't') {
-            $data_approval['status'] = 't';
+        // if ($status == 't') {
+        //     $data_approval['status'] = 't';
+        //     $store_approval = PerbaikanPersetujuan::create($data_approval);
+        //     // return $data_approval;
+        //     // return redirect()->route('checking.serviceorder')->with('success', 'Service Order is Rejected');
+        //     return redirect()->back()->with('success', 'Approval is Rejected');
+        // } else if ($status == 's') {
+        DB::beginTransaction();
+        try {
+            $data_approval['status'] = 's';
             $store_approval = PerbaikanPersetujuan::create($data_approval);
-            // return $data_approval;
-            // return redirect()->route('checking.serviceorder')->with('success', 'Service Order is Rejected');
-            return redirect()->back()->with('success', 'Approval is Rejected');
-        } else if ($status == 's') {
-            DB::beginTransaction();
-            try {
-                $data_approval['status'] = 's';
-                $store_approval = PerbaikanPersetujuan::create($data_approval);
-                $data_repair = [
-                    'id_persetujuan' => $store_approval->id_persetujuan,
-                    'id_dealer' => $request->id_dealer,
-                    'tgl_perbaikan' => $request->tgl_perbaikan,
-                    'tgl_selesai' => $request->tgl_selesai,
-                    'status_pebaikan' => 'p'
+            $data_repair = [
+                'id_persetujuan' => $store_approval->id_persetujuan,
+                'id_dealer' => $request->id_dealer,
+                'tgl_perbaikan' => $request->tgl_perbaikan,
+                'tgl_selesai' => $request->tgl_selesai,
+                'status_pebaikan' => 'p'
+            ];
+            $store_repair = Perbaikan::create($data_repair);
+            $get_detail =  DB::table('tb_detail_pengecekan')
+                ->select(
+                    'tb_detail_pengecekan.id_detail_pengecekan',
+                    'tb_jenis_pengecekan.jenis_pengecekan as komponen',
+                )
+                ->join('tb_jenis_pengecekan', 'tb_jenis_pengecekan.id_jenis_pengecekan', '=', 'tb_detail_pengecekan.id_jenis_pengecekan')
+                ->where('id_pengecekan', $id_pengecekan)
+                ->get();
+            foreach ($get_detail as $key => $de) {
+                $data_detail = [
+                    'id_perbaikan' => $store_repair->id_perbaikan,
+                    'tgl_pergantian' => $request->tgl_perbaikan,
+                    'nama_komponen' => $de->komponen,
+                    'jml_komponen' => 1
                 ];
-                $store_repair = Perbaikan::create($data_repair);
-                $get_detail =  DB::table('tb_detail_pengecekan')
-                    ->select(
-                        'tb_detail_pengecekan.id_detail_pengecekan',
-                        'tb_jenis_pengecekan.jenis_pengecekan as komponen',
-                    )
-                    ->join('tb_jenis_pengecekan', 'tb_jenis_pengecekan.id_jenis_pengecekan', '=', 'tb_detail_pengecekan.id_jenis_pengecekan')
-                    ->where('id_pengecekan', $id_pengecekan)
-                    ->get();
-                foreach ($get_detail as $key => $de) {
-                    $data_detail = [
-                        'id_perbaikan' => $store_repair->id_perbaikan,
-                        'tgl_pergantian' => $request->tgl_perbaikan,
-                        'nama_komponen' => $de->komponen,
-                        'jml_komponen' => 1
-                    ];
-                    $store_detail = DB::table('tb_detail_pergantian')->insert($data_detail);
-                }
-
-                DB::commit();
-                return redirect()->back()->with('success', 'Approval is Accepted');
-                // return response()->json(
-                //     [
-                //         'pesan' => 'sukses',
-                //         'setuju' => $store_approval,
-                //         'baik' => $data_repair,
-                //         'detail' => $get_detail
-                //     ]
-                // );
-            } catch (\Exception $exception) {
-                //throw $th;
-                DB::rollBack();
-                return redirect()->back()->with('success', 'Some Errors ' . $exception);
-                // return response()->json(
-                //     [
-                //         'pesan' => 'gagal',
-                //         'errors' => $exception
-                //     ]
-                // );
+                $store_detail = DB::table('tb_detail_pergantian')->insert($data_detail);
             }
+
+            DB::commit();
+            return redirect()->back()->with('success', 'Persetujuan perbaikan berhasil dibuat.');
+            // return response()->json(
+            //     [
+            //         'pesan' => 'sukses',
+            //         'setuju' => $store_approval,
+            //         'baik' => $data_repair,
+            //         'detail' => $get_detail
+            //     ]
+            // );
+        } catch (\Exception $exception) {
+            //throw $th;
+            DB::rollBack();
+            return redirect()->back()->with('success', 'Gagal, persetujuan perbaikan gagal dibuat.');
+            // return response()->json(
+            //     [
+            //         'pesan' => 'gagal',
+            //         'errors' => $exception
+            //     ]
+            // );
+        }
+        // }
+    }
+
+    public function reject(Request $request)
+    {
+        $id_petugas = 4;
+        $data_approval = [
+            'id_pengecekan' => $request->get('id_pengecekan'),
+            'id_petugas' => $id_petugas,
+            'tgl_persetujuan' => Carbon::now()->format('Y-m-d'),
+            'status' => 't'
+        ];
+        $reject = PerbaikanPersetujuan::create($data_approval);
+        if ($reject) {
+            return redirect()->back()->with('success', 'Berhasil tolak perbaikan.');
+        } else {
+            return redirect()->back()->with('success', 'Gagal tolak perbaikan!');
         }
     }
 }
