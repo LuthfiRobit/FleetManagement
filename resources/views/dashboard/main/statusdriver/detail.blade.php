9b3d9bcb68e3177a2079b8d8780e532e4ab611b9
LuthfiRobit <luthfirobit@gmail.com>
2022-02-21 23:36:19 +0700

    make view status driver

---
+++ resources/views/dashboard/main/statusdriver/detail.blade.php
@@ -0,0 +1,230 @@
+@extends('layouts.backend.main')
+
+@section('title','Laporan Penugasan | Detail')
+
+@section('content')
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Post-->
+    <div class="post d-flex flex-column-fluid" id="kt_post">
+        <!--begin::Container-->
+        <div id="kt_content_container" class="container-xxl">
+            <div class="card mb-5 mb-xl-8">
+                <!--begin::Header-->
+                <div class="card-header border-0">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bolder fs-3 mb-1">DETAIL STATUS DRIVER</span>
+                        <span class="text-muted mt-1 fw-bold fs-7">Driver : {{$status->nama_driver}}</span>
+                    </h3>
+                </div>
+            </div>
+            <!--begin::Layout-->
+            <div class="d-flex flex-column flex-lg-row">
+                <!--begin::Content-->
+                <div class="flex-lg-row-fluid me-lg-15 order-2 order-lg-1 mb-10 mb-lg-0">
+                    <!--begin::Card-->
+                    <div class="card card-flush pt-3 mb-5 mb-xl-10">
+                        <!--begin::Card header-->
+                        <div class="card-header">
+                            <!--begin::Card title-->
+                            <div class="card-title">
+                                <h2 class="fw-bolder">STATUS DRIVER</h2>
+                            </div>
+                            <!--begin::Card title-->
+                            <div class="card-toolbar">
+                                @if ($status->status == 'r')
+                                <div class=" bg-light-primary rounded border-primary border border-dashed p-2">
+                                    AKTIF
+                                </div>
+                                @elseif ($status->status == 'n')
+                                <div class=" bg-light-warning rounded border-warning border border-dashed p-2">
+                                    NON AKTIF
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                        <!--end::Card header-->
+                        <!--begin::Card body-->
+                        <div class="card-body pt-3">
+                            <!--begin::Section-->
+                            <div class="mb-10">
+                                <!--begin::Title-->
+                                <h5 class="mb-4">Detail:</h5>
+                                <!--end::Title-->
+                                <!--begin::Details-->
+                                <div class="d-flex flex-wrap py-5">
+                                    <!--begin::Row-->
+                                    <div class="flex-equal me-5">
+                                        <!--begin::Details-->
+                                        <table class="table fs-6 fw-bold gs-0 gy-2 gx-2 m-0">
+                                            <!--begin::Row-->
+                                            <tr>
+                                                <td class="text-gray-400 min-w-175px w-175px">Tanggal Non Aktif:</td>
+                                                <td class="text-gray-800 min-w-200px">
+                                                    {{Carbon\Carbon::parse($status->tgl_nonaktif)->format('d F Y')}}
+                                                </td>
+                                            </tr>
+                                            <!--end::Row-->
+                                            <!--begin::Row-->
+                                            <tr>
+                                                <td class="text-gray-400 min-w-175px w-175px">Tanggal Aktif:</td>
+                                                <td class="text-gray-800 min-w-200px">
+                                                    @if ($status->tgl_aktif != null)
+                                                    {{Carbon\Carbon::parse($status->tgl_aktif)->format('d F Y')}}
+                                                    @else
+                                                    ---
+                                                    @endif
+
+                                                </td>
+                                            </tr>
+                                            <!--end::Row-->
+                                            <!--begin::Row-->
+                                            <tr>
+                                                <td class="text-gray-400">Lama Non Aktif:</td>
+                                                <td class="text-gray-800">
+                                                    @if ($status->jml_nonaktif != null)
+                                                    {{$status->jml_nonaktif}}
+                                                    @else
+                                                    ---
+                                                    @endif
+
+                                                    Hari</td>
+                                            </tr>
+                                            <!--end::Row-->
+                                        </table>
+                                        <!--end::Details-->
+                                    </div>
+                                    <!--end::Row-->
+                                    <!--begin::Row-->
+                                    <div class="flex-equal">
+                                        <!--begin::Details-->
+                                        <table class="table fs-6 fw-bold gs-0 gy-2 gx-2 m-0">
+                                            <!--begin::Row-->
+                                            <tr>
+                                                <td class="text-gray-400 min-w-175px w-175px">Status:</td>
+                                                <td class="text-gray-800 min-w-200px">
+                                                    @if ($status->status == 'r')
+                                                    <div class="badge badge-lg badge-light-primary d-inline">
+                                                        AKTIF
+                                                    </div>
+                                                    @elseif ($status->status == 'n')
+                                                    <div class=" badge badge-lg badge-light-warning d-inline">
+                                                        NON AKTIF
+                                                    </div>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                            <!--end::Row-->
+                                            <!--begin::Row-->
+                                            <tr>
+                                                <td class="text-gray-400">Keterangan:</td>
+                                                <td class="text-gray-800">{{$status->keterangan}}</td>
+                                            </tr>
+                                            <!--end::Row-->
+                                            <!--begin::Row-->
+                                            <tr>
+                                                <td class="text-gray-400">Bukti:</td>
+                                                <td class="text-gray-800">
+                                                    <div class="col-lg-3 col-md-2 mb-0 mb-lg-3">
+                                                        <div class="bg-image hover-overlay  bg-light-primary rounded border-primary border border-dashed"
+                                                            data-ripple-color="light">
+                                                            <img src="{{url('/assets/img_bukti/'.$status->foto_bukti)}}"
+                                                                class="w-100" />
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <!--end::Row-->
+                                        </table>
+                                        <!--end::Details-->
+                                    </div>
+                                    <!--end::Row-->
+                                </div>
+                                <!--end::Row-->
+                            </div>
+                            <!--end::Section-->
+                        </div>
+                        <!--end::Card body-->
+                    </div>
+                    <!--end::Card-->
+                </div>
+                <!--end::Content-->
+                <!--begin::Sidebar-->
+                <div class="flex-column flex-lg-row-auto w-lg-250px w-xl-300px mb-10 order-1 order-lg-2">
+                    <!--begin::Card-->
+                    <div class="card card-flush mb-0" data-kt-sticky="true" data-kt-sticky-name="subscription-summary"
+                        data-kt-sticky-offset="{default: false, lg: '200px'}"
+                        data-kt-sticky-width="{lg: '250px', xl: '300px'}" data-kt-sticky-left="auto"
+                        data-kt-sticky-top="150px" data-kt-sticky-animation="false" data-kt-sticky-zindex="95">
+                        <!--begin::Card header-->
+                        <div class="card-header">
+                            <!--begin::Card title-->
+                            <div class="card-title">
+                                <h2>DRIVER</h2>
+                            </div>
+                            <!--end::Card title-->
+                            <!--begin::Card toolbar-->
+                            <!--end::Card toolbar-->
+                        </div>
+                        <!--end::Card header-->
+                        <!--begin::Card body-->
+                        <div class="card-body pt-0 fs-6">
+                            <!--begin::Section-->
+                            <div class="mb-7">
+                                <!--begin::Details-->
+                                <div class="d-flex align-items-center">
+                                    <!--begin::Avatar-->
+                                    <div class="symbol symbol-60px symbol-circle me-3">
+                                        <img alt="Pic" src="{{url('assets/backend/assets/media/avatars/150-4.jpg')}}" />
+                                    </div>
+                                    <!--end::Avatar-->
+                                    <!--begin::Info-->
+                                    <div class="d-flex flex-column">
+                                        <!--begin::Name-->
+                                        <a href="#" class="fs-4 fw-bolder text-gray-900 text-hover-primary me-2">
+                                            {{$status->nama_driver}}
+                                        </a>
+                                        <!--end::Name-->
+                                        <!--begin::Email-->
+                                        <a href="#"
+                                            class="fw-bold text-gray-600 text-hover-primary">{{$status->no_tlp}}</a>
+                                        <!--end::Email-->
+                                        <span class="badge badge-light-primary">{{$status->departemen}}</span>
+                                    </div>
+                                    <!--end::Info-->
+                                </div>
+                                <!--end::Details-->
+                            </div>
+                            <!--end::Section-->
+                            <!--begin::Seperator-->
+                            <div class="separator separator-dashed mb-7"></div>
+                            <!--end::Seperator-->
+                            <!--begin::Section-->
+                            <table class="table fs-6 fw-bold gs-0 gy-2 gx-2">
+                                <!--begin::Row-->
+                                <tr class="">
+                                    <td class="text-gray-400">History Non Aktif:</td>
+                                    <td class="text-gray-800">{{$history}} hari</td>
+                                </tr>
+                            </table>
+                            <!--end::Section-->
+                        </div>
+                        <!--end::Card body-->
+                    </div>
+                    <!--end::Card-->
+                </div>
+                <!--end::Sidebar-->
+            </div>
+            <!--end::Layout-->
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::Post-->
+</div>
+@endsection
+
+@push('scripts')
+
+<script text="text/javascript">
+    "use strict";
+</script>
+@endpush
---
+++ resources/views/dashboard/main/statusdriver/index.blade.php
@@ -0,0 +1,211 @@
+@extends('layouts.backend.main')
+
+@section('title','Laporan Status Driver | Utama')
+@section('style-on-this-page-only')
+<link href="{{url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet"
+    type="text/css" />
+@endsection
+
+@section('content')
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Post-->
+    <div class="post d-flex flex-column-fluid" id="kt_post">
+        <!--begin::Container-->
+        <div id="kt_content_container" class="container-xxl">
+            <div class="card mb-5 mb-xl-8">
+                <!--begin::Header-->
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bolder fs-3 mb-1">LAPORAN STATUS DRIVER</span>
+                        <span class="text-muted mt-1 fw-bold fs-7">Ada
+                            {{$status->where('status','n')->count()}} sedang non aktif</span>
+                    </h3>
+                    <ul
+                        class="align-items-end  nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-bold mb-8">
+                        <!--begin:::Tab item-->
+                        <li class="nav-item">
+                            <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab"
+                                href="#tab_avaliable">NON AKTIF</a>
+                        </li>
+                        <!--end:::Tab item-->
+                        <!--begin:::Tab item-->
+                        <li class="nav-item">
+                            <a class="nav-link text-active-primary pb-4" data-kt-countup-tabs="true"
+                                data-bs-toggle="tab" href="#tab_unavaliable">AKTIF</a>
+                        </li>
+                        <!--end:::Tab item-->
+                    </ul>
+                </div>
+
+                <!--end::Header-->
+                <!--begin::Body-->
+                <div class="card-body py-3">
+
+                    @if(session()->has('success'))
+                    <!--begin::Alert-->
+                    <div
+                        class="alert alert-dismissible bg-light-primary border border-primary border-dashed d-flex flex-column flex-sm-row w-100 p-5 mb-10">
+                        <!--begin::Icon-->
+                        <!--begin::Svg Icon | path: icons/duotune/communication/com003.svg-->
+                        <span class="svg-icon svg-icon-2hx svg-icon-primary me-4 mb-5 mb-sm-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <i class="fa fa-check" aria-hidden="true"></i>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                        <!--end::Icon-->
+                        <!--begin::Content-->
+                        <div class="d-flex flex-column pe-0 pe-sm-10">
+                            <h5 class="mb-1">Success</h5>
+                            <span> {{ session()->get('success') }}</span>
+                        </div>
+                        <!--end::Content-->
+                        <!--begin::Close-->
+                        <button type="button"
+                            class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto"
+                            data-bs-dismiss="alert">
+                            <i class="bi bi-x fs-1 text-danger"></i>
+                        </button>
+                        <!--end::Close-->
+                    </div>
+                    <!--end::Alert-->
+                    @endif
+
+                    <div class="tab-content" id="myTabContent">
+                        <!--begin:::Tab pane-->
+                        <div class="tab-pane fade show active" id="tab_avaliable" role="tabpanel">
+                            <!--begin::Table container-->
+                            <table id="kt_datatable_nonaktif"
+                                class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4 display responsive nowr">
+                                <thead>
+                                    <tr class="fw-bolder fs-6 text-gray-800 px-7">
+                                        <th>No.</th>
+                                        <th>No. Status</th>
+                                        <th>Driver</th>
+                                        <th>Tgl. Non Aktif</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($status->where('status','n') as $sd)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>SD_{{$sd->id_status}}</td>
+                                        <td>{{$sd->nama_driver}}</td>
+                                        <td>{{$sd->tgl_nonaktif}}</td>
+                                        <td>
+                                            @if ($sd->status == 'n')
+                                            <span class="badge badge-light-danger">Non Aktif</span>
+                                            @else
+                                            <span class="badge badge-light-primary">Aktif</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{route('status.detail', $sd->id_status)}}"
+                                                class="btn btn-light bnt-active-light-primary btn-sm">Detail</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            <!--end::Table container-->
+                        </div>
+                        <!--begin:::Tab pane-->
+                        <div class="tab-pane fade show " id="tab_unavaliable" role="tabpanel">
+                            <!--begin::Table container-->
+                            <table id="kt_datatable_aktif"
+                                class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4 display responsive nowr">
+                                <thead>
+                                    <tr class="fw-bolder fs-6 text-gray-800 px-7">
+                                        <th>No.</th>
+                                        <th>No. Status</th>
+                                        <th>Driver</th>
+                                        <th>Tgl. Non Aktif</th>
+                                        <th>Tgl. Aktif</th>
+                                        <th>Jml. Hari</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($status->where('status','r') as $sd)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>SD_{{$sd->id_status}}</td>
+                                        <td>{{$sd->nama_driver}}</td>
+                                        <td>{{$sd->tgl_nonaktif}}</td>
+                                        <td>{{$sd->tgl_aktif}}</td>
+                                        <td>{{$sd->jml_nonaktif}}</td>
+                                        <td>
+                                            @if ($sd->status == 'n')
+                                            <span class="badge badge-light-danger">Non Aktif</span>
+                                            @else
+                                            <span class="badge badge-light-primary">Aktif</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{route('status.detail', $sd->id_status)}}"
+                                                class="btn btn-light bnt-active-light-primary btn-sm">Detail</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            <!--end::Table container-->
+                        </div>
+                    </div>
+                </div>
+                <!--begin::Body-->
+            </div>
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::Post-->
+</div>
+@endsection
+
+@push('scripts')
+<!--begin::Page Vendors Javascript(used by this page)-->
+<script src="{{ url('assets/backend/assets/plugins/custom/prismjs/prismjs.bundle.js') }}"></script>
+<script src="{{ url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.js') }}">
+</script>
+<!--end::Page Vendors Javascript-->
+<script text="text/javascipt">
+    $("#kt_datatable_nonaktif").DataTable({
+        "language": {
+            "lengthMenu": "Show _MENU_",
+        },
+        "dom":
+            "<'row'" +
+            "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+            "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+            ">" +
+
+            "<'table-responsive'tr>" +
+
+            "<'row'" +
+            "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+            "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+            ">"
+    });
+    $("#kt_datatable_aktif").DataTable({
+        "language": {
+            "lengthMenu": "Show _MENU_",
+        },
+        "dom":
+            "<'row'" +
+            "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+            "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+            ">" +
+
+            "<'table-responsive'tr>" +
+
+            "<'row'" +
+            "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+            "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+            ">"
+    });
+</script>
+@endpush
