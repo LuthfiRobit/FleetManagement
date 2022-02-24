2c7216c81442ca7d0bb74092f0c6d27ed2fe3325
LuthfiRobit <luthfirobit@gmail.com>
2022-02-24 12:43:58 +0700

    fixing view index

---
+++ resources/views/dashboard/main/rating/index.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.backend.main')
+
+@section('title','Laporan Kinerja Driver | Main')
+@section('style-on-this-page-only')
+<link href="{{url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet"
+    type="text/css" />
+@endsection
+
+@section('content')
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+    <!--begin::Post-->
+    <div class="post d-flex flex-column-fluid" id="kt_post">
+        <!--begin::Container-->
+        <div id="kt_content_container" class="container-xxl">
+            <div class="card mb-5 mb-xl-8">
+                <!--begin::Header-->
+                <div class="card-header border-0 pt-5">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bolder fs-3 mb-1">LAPORAN KINERJA DRIVER</span>
+                        {{-- <span class="text-muted mt-1 fw-bold fs-7">Lebih dari 2 Jabatan</span> --}}
+                    </h3>
+                    {{-- <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top"
+                        data-bs-trigger="hover" title="" data-bs-original-title="Tekan untuk menambah jabatan">
+                        <a href="#" class="btn btn-sm btn-light btn-active-primary" data-bs-toggle="modal"
+                            data-bs-target="#kt_modal_new_feul">
+                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                            <span class="svg-icon svg-icon-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1"
+                                        transform="rotate(-90 11.364 20.364)" fill="black"></rect>
+                                    <rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black"></rect>
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->Tambah Jabatan
+                        </a>
+                    </div> --}}
+                </div>
+                <!--end::Header-->
+                <!--begin::Body-->
+                <div class="card-body py-3">
+                    <!--begin::Table container-->
+                    <table id="kt_datatable_example_5"
+                        class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4 display responsive nowr">
+                        <thead>
+                            <tr class="fw-bolder fs-6 text-gray-800 px-7">
+                                <th>No</th>
+                                <th>Nama Driver</th>
+                                <th>Departemen</th>
+                                <th>Rating</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($rating as $rt)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$rt->nama_driver}}</td>
+                                <td>{{$rt->departemen}}</td>
+                                <td>
+                                    @for ($i = 0; $i < $rt->rating; $i++)
+                                        <i class="bi bi-star-fill fs-2x" style="color:#ffad0f"></i>
+                                        @endfor
+                                </td>
+                                <td>
+                                    <a href="" class="btn btn-light bnt-active-light-primary btn-sm">Detail</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <!--end::Table container-->
+                </div>
+                <!--begin::Body-->
+            </div>
+        </div>
+        <!--end::Container-->
+    </div>
+
+    <!--end::Post-->
+</div>
+@endsection
+
+@push('scripts')
+<!--begin::Page Vendors Javascript(used by this page)-->
+<script src="{{ url('assets/backend/assets/plugins/custom/prismjs/prismjs.bundle.js') }}"></script>
+<script src="{{ url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+<!--end::Page Vendors Javascript-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="{{ url('assets/backend/assets/js/custom/documentation/documentation.js') }}"></script>
+<script src="{{ url('assets/backend/assets/js/custom/documentation/search.js') }}"></script>
+{{-- <script src="{{ url('assets/backend/assets/js/custom/documentation/general/datatables/advanced.js') }}"></script>
+--}}
+<!--end::Page Custom Javascript-->
+<script text="text/javascipt">
+    $("#kt_datatable_example_5").DataTable({
+        "language": {
+            "lengthMenu": "Show _MENU_",
+        },
+        "dom":
+            "<'row'" +
+            "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+            "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+            ">" +
+
+            "<'table-responsive'tr>" +
+
+            "<'row'" +
+            "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+            "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+            ">"
+    });
+
+    "use strict";
+</script>
+@endpush
