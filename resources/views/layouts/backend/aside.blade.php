1dbde4e1de0388fa70c2b08562a9c7a90447669a
LuthfiRobit <luthfirobit@gmail.com>
2022-02-28 17:38:43 +0700

    add aside dashboard main

--- resources/views/layouts/backend/aside.blade.php
+++ resources/views/layouts/backend/aside.blade.php
@@ -60,6 +60,25 @@
                             </span>
                             <!--end::Svg Icon-->
                         </span>
+                        <span class="menu-title">Main Dashboard</span>
+                    </a>
+                </div>
+                <div class="menu-item">
+                    <a class="menu-link {{ Request::path() == 'monitoring/driver' ? 'active' : '' }}"
+                        href="{{route('dashboard.monitoring.driver')}}">
+                        <span class="menu-icon">
+                            <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+                            <span class="svg-icon svg-icon-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <rect x="2" y="2" width="9" height="9" rx="2" fill="black" />
+                                    <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="black" />
+                                    <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="black" />
+                                    <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="black" />
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->
+                        </span>
                         <span class="menu-title">Monitoring Driver</span>
                     </a>
                 </div>
