8c380558c95917c675ed7063feaca03987c4278d
LuthfiRobit <luthfirobit@gmail.com>
2022-02-28 10:21:18 +0700

    comment script widget

--- resources/views/layouts/backend/script.blade.php
+++ resources/views/layouts/backend/script.blade.php
@@ -8,7 +8,7 @@
 <!--end::Page Vendors Javascript-->
 <!--begin::Page Custom Javascript(used by this page)-->
 {{-- <script src="{{url('assets/backend/assets/js/custom/modals/new-target.js')}}"></script> --}}
-<script src="{{ url('assets/backend/assets/js/custom/widgets.js') }}"></script>
+{{-- <script src="{{ url('assets/backend/assets/js/custom/widgets.js') }}"></script> --}}
 <script src="{{ url('assets/backend/assets/js/custom/apps/chat/chat.js') }}"></script>
 <script src="{{ url('assets/backend/assets/js/custom/modals/create-app.js') }}"></script>
 <script src="{{ url('assets/backend/assets/js/custom/modals/upgrade-plan.js') }}"></script>
