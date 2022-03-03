2b1f82365ecfbf8b2ee2139930949f9df4d34b49
LuthfiRobit <luthfirobit@gmail.com>
2022-03-03 20:55:27 +0700

    fixing dashboard controller

--- app/Http/Controllers/Main/DashboardController.php
+++ app/Http/Controllers/Main/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Driver;
+use App\Models\Kendaraan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,8 +12,89 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-
-        return view('dashboard.dashboard');
+        $countDriver = Driver::where('status_driver', 'y')->count();
+        $countKendaraan = Kendaraan::where('status', 'y')->count();
+        $penugasan = DB::table('tb_penugasan_driver')
+            ->select(
+                'tb_penugasan_driver.tmp_tujuan as tujuan',
+                'tb_driver.nama_driver'
+            )
+            ->leftJoin('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->where('tb_penugasan_driver.status_penugasan', 'p')
+            ->orderByDesc('tb_penugasan_driver.id_do')
+            ->limit(5)
+            ->get();
+        $status = DB::table('tb_status_driver')
+            ->select(
+                'tb_status_driver.status',
+                'tb_driver.no_badge',
+                'tb_driver.nama_driver'
+            )
+            ->leftJoin('tb_driver', 'tb_driver.id_driver', '=', 'tb_status_driver.id_driver')
+            ->where('tb_status_driver.status', 'n')
+            ->orderByDesc('tb_status_driver.id_status')
+            ->limit(3)
+            ->get();
+        $perbaikan = DB::table('tb_perbaikan')
+            ->select(
+                'tb_perbaikan.status_perbaikan',
+                'tb_kendaraan.kode_asset',
+                'tb_kendaraan.nama_kendaraan'
+            )
+            ->leftJoin('tb_persetujuan_perbaikan', 'tb_persetujuan_perbaikan.id_persetujuan', '=', 'tb_perbaikan.id_persetujuan')
+            ->leftJoin('tb_pengecekan_kendaraan', 'tb_pengecekan_kendaraan.id_pengecekan', '=', 'tb_persetujuan_perbaikan.id_pengecekan')
+            ->leftJoin('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_pengecekan_kendaraan.id_kendaraan')
+            ->where('tb_perbaikan.status_perbaikan', 'p')
+            ->orderByDesc('tb_perbaikan.id_perbaikan')
+            ->limit(3)
+            ->get();
+        // $history = DB::table('tb_driver')
+        //     ->selectRaw(
+        //         "tb_driver.no_badge,
+        //     tb_driver.nama_driver,
+        //     COUNT(tb_penugasan_driver.id_do) as penugasan,
+        //     COUNT(tb_pembatalan_penugasan.id_driver) as pembatalan,
+        //     SUM(tb_status_driver.jml_nonaktif) as status,
+        //     CEIL((SELECT AVG(tb_rating_driver.nilai) as x FROM tb_rating_driver WHERE tb_rating_driver.id_do = tb_penugasan_driver.id_do)) as rating
+        //     GROUP BY penugasan
+        //     "
+        //     )
+        //     ->leftJoin('tb_penugasan_driver', 'tb_penugasan_driver.id_driver', '=', 'tb_driver.id_driver')
+        //     ->leftJoin('tb_pembatalan_penugasan', 'tb_pembatalan_penugasan.id_driver', '=', 'tb_driver.id_driver')
+        //     ->leftJoin('tb_status_driver', 'tb_status_driver.id_driver', '=', 'tb_driver.id_driver')
+        //     ->leftJoin('tb_kecelakaan', 'tb_kecelakaan.id_do', '=', 'tb_penugasan_driver.id_do')
+        //     // ->groupBy('id_driver')
+        //     ->orderByDesc('penugasan')
+        //     ->limit(5)
+        //     ->get();
+        $history = DB::select(
+            "SELECT tb_driver.id_driver, tb_driver.no_badge,
+            tb_driver.nama_driver,
+            COUNT(tb_penugasan_driver.id_do) as penugasan,
+            COUNT(tb_pembatalan_penugasan.id_driver) as pembatalan,
+            COUNT(tb_kecelakaan.id_do) as kecelakaan,
+            SUM(tb_status_driver.jml_nonaktif) as nonaktif,
+            CEIL((SELECT AVG(tb_rating_driver.nilai) as x FROM tb_rating_driver WHERE tb_rating_driver.id_do = tb_penugasan_driver.id_do)) as rating
+            FROM tb_driver
+            LEFT JOIN tb_penugasan_driver on tb_penugasan_driver.id_driver = tb_driver.id_driver
+            LEFT JOIN tb_pembatalan_penugasan on tb_pembatalan_penugasan.id_driver = tb_driver.id_driver
+            LEFT JOIN tb_status_driver on tb_status_driver.id_driver = tb_driver.id_driver
+            LEFT JOIN tb_kecelakaan on tb_kecelakaan.id_do = tb_penugasan_driver.id_do
+            GROUP BY tb_driver.id_driver, tb_driver.no_badge, tb_driver.nama_driver, .tb_penugasan_driver.id_do
+            ORDER BY rating DESC
+            LIMIT 5
+            "
+        );
+        $data = [
+            'driver' => $countDriver,
+            'kendaraan' => $countKendaraan,
+            'penugasan' => $penugasan,
+            'status' => $status,
+            'perbaikan' => $perbaikan,
+            'history' => $history
+        ];
+        // return $data;
+        return view('dashboard.dashboard', $data);
     }
 
     public function monitoring()
