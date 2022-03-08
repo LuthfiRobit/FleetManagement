d6bbda199e8c8dd250a2e55deb15b3fa111b77c8
LuthfiRobit <luthfirobit@gmail.com>
2022-03-08 07:07:34 +0700

    fixing revisi point 2

--- resources/views/dashboard/main/serviceorder/accept.blade.php
+++ resources/views/dashboard/main/serviceorder/accept.blade.php
@@ -54,7 +54,7 @@
                                 <select class="form-select form-select-solid" data-control="select2"
                                     data-hide-search="false" data-placeholder="Pilih Kendaraan" id="id_kendaraan"
                                     name="id_kendaraan">
-                                    <option value="">Pilih Kendaraan</option>
+                                    {{-- <option value="">Pilih Kendaraan</option> --}}
                                     @foreach ($kendaraan as $kd)
                                     <option value="{{$kd->id_kendaraan}}" data-sim="{{$kd->id_jenis_sim}}">
                                         {{$kd->alokasi}} | {{$kd->nama_kendaraan}} | {{$kd->no_polisi}} |
@@ -69,6 +69,9 @@
                                     data-hide-search="false" data-placeholder="Pilih Driver" id="id_driver"
                                     name="id_driver">
                                     {{-- <option value="">Pilih Driver</option> --}}
+                                    @foreach ($driver as $dr)
+                                    <option value="{{$dr->id_driver}}">{{$dr->nama_driver}}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
@@ -135,48 +138,48 @@
 
 <!--end::Page Vendors Javascript-->
 <script text="text/javascipt">
-    $(function () {
-        $('#id_kendaraan').on('change', function () {
-        var id_sim =  $(this).val();
-            // alert($(this).find(':selected').data('sim'));
-            $.ajax({
-                // url: 'http://127.0.0.1:8000/driver/select?id_sim='+id_sim+'&id_so='+{{$so->id_so}},
-                url : '{{route("driver.select")}}',
-                method: 'GET',
-                // error: (result) => $.Notification.error(result),
-                data: {
-                    // 'id_sim': $(this).val(),
-                    'id_sim': $(this).find(':selected').data('sim'),
-                    'id_so': {{$so->id_so}}
-                    },
-                dataType: 'json',
-                success: function (result) {
-                    console.log(result);
-                    console.log(result.Success);
-                    console.log(result.Driver);
-                    if (result.Success == true) {
-                        $('#id_driver').empty();
-                        var driver = result.Driver;
-                        $.each(driver, function (key, item) {
-                            $('#id_driver').append($('<option></option>').attr('value', item.id_driver).text(item.nama_driver));
-                                // $('option', this).each(function () {
-                                //     if ($(this).html() == item.nama_driver) {
-                                //         $(this).attr('selected', 'selected')
-                                //     };
-                                // });
-                        })
-                    } else {
-                        alert(result.Message);
-                        $('#id_driver').empty();
-                    }
-                }
-            })
-        });
-        // $('#id_driver').on('change', function () {
-        //     id_sim =  $(this).val();
-        //     alert(id_sim);
-        // });
-    });
+    // $(function () {
+    //     $('#id_kendaraan').on('change', function () {
+    //     var id_sim =  $(this).val();
+    //         // alert($(this).find(':selected').data('sim'));
+    //         $.ajax({
+    //             // url: 'http://127.0.0.1:8000/driver/select?id_sim='+id_sim+'&id_so='+{{$so->id_so}},
+    //             url : '{{route("driver.select")}}',
+    //             method: 'GET',
+    //             // error: (result) => $.Notification.error(result),
+    //             data: {
+    //                 // 'id_sim': $(this).val(),
+    //                 'id_sim': $(this).find(':selected').data('sim'),
+    //                 'id_so': {{$so->id_so}}
+    //                 },
+    //             dataType: 'json',
+    //             success: function (result) {
+    //                 console.log(result);
+    //                 console.log(result.Success);
+    //                 console.log(result.Driver);
+    //                 if (result.Success == true) {
+    //                     $('#id_driver').empty();
+    //                     var driver = result.Driver;
+    //                     $.each(driver, function (key, item) {
+    //                         $('#id_driver').append($('<option></option>').attr('value', item.id_driver).text(item.nama_driver));
+    //                             // $('option', this).each(function () {
+    //                             //     if ($(this).html() == item.nama_driver) {
+    //                             //         $(this).attr('selected', 'selected')
+    //                             //     };
+    //                             // });
+    //                     })
+    //                 } else {
+    //                     alert(result.Message);
+    //                     $('#id_driver').empty();
+    //                 }
+    //             }
+    //         })
+    //     });
+    //     // $('#id_driver').on('change', function () {
+    //     //     id_sim =  $(this).val();
+    //     //     alert(id_sim);
+    //     // });
+    // });
 
     var KTFormAccept = function () {
         var t, e, n, a, i;
