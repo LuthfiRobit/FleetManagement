5861203df04f1e253ae9b59dd6e661a6efbb2b2c
LuthfiRobit <luthfirobit@gmail.com>
2022-05-22 09:36:38 +0700

    add export penugasan function

--- app/Http/Controllers/Main/PenugasanDriverController.php
+++ app/Http/Controllers/Main/PenugasanDriverController.php
@@ -7,6 +7,8 @@ use App\Models\PenugasanBatal;
 use App\Models\PenugasanDriver;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
+use Carbon\Carbon;
 
 class PenugasanDriverController extends Controller
 {
@@ -208,6 +210,113 @@ class PenugasanDriverController extends Controller
         } else {
             // return $find;
             return redirect()->back()->with('success', 'Tolak Pembatalan Gagal Diproses');
+        }
+    }
+
+    public function exportPdfPenugasan($id)
+    {
+        $data['detail'] = DB::table('tb_penugasan_driver')
+            ->select(
+                'tb_penugasan_driver.id_do',
+                'tb_penugasan_driver.id_service_order',
+                'tb_penugasan_driver.id_driver',
+                'tb_penugasan_driver.tgl_acc',
+                'tb_penugasan_driver.tgl_penugasan',
+                'tb_penugasan_driver.jam_berangkat',
+                'tb_penugasan_driver.kembali',
+                'tb_penugasan_driver.status_penugasan as status_do',
+                'tb_order_kendaraan.tempat_penjemputan',
+                'tb_order_kendaraan.tujuan',
+                'tb_order_kendaraan.no_so',
+                'tb_petugas.nama_lengkap as nama_petugas',
+                'tb_driver.nama_driver',
+                'tb_petugas.no_tlp as p_tlp',
+                'tb_kendaraan.nama_kendaraan',
+                'tb_kendaraan.no_polisi',
+                'tb_kendaraan.kode_asset',
+                'tb_kendaraan.warna',
+                'tb_kendaraan.jenis_penggerak',
+                'tb_merk_kendaraan.nama_merk as merk',
+                'tb_jenis_kendaraan.nama_jenis as jenis',
+                'tb_bahan_bakar.nama_bahan_bakar as bahan_bakar'
+            )
+            ->leftJoin('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->leftJoin('tb_petugas', 'tb_petugas.id_petugas', '=', 'tb_penugasan_driver.id_petugas')
+            ->leftJoin('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->rightJoin('tb_order_kendaraan', 'tb_order_kendaraan.id_service_order', '=', 'tb_penugasan_driver.id_service_order')
+            ->leftJoin('tb_bahan_bakar', 'tb_bahan_bakar.id_bahan_bakar', '=', 'tb_kendaraan.id_bahan_bakar')
+            ->leftJoin('tb_merk_kendaraan', 'tb_merk_kendaraan.id_merk', '=', 'tb_kendaraan.id_merk')
+            ->leftJoin('tb_jenis_kendaraan', 'tb_jenis_kendaraan.id_jenis_kendaraan', '=', 'tb_kendaraan.id_jenis_kendaraan')
+            ->where('id_do', $id)
+            ->orderByDesc('id_do')
+            ->first();
+
+        $data['driver'] = DB::table('tb_driver')
+            ->select(
+                'tb_driver.nama_driver',
+                'tb_driver.no_tlp as d_tlp',
+                'tb_driver.foto_driver',
+                'tb_departemen.nama_departemen as departemen'
+            )
+            ->leftJoin('tb_departemen', 'tb_departemen.id_departemen', '=', 'tb_driver.id_departemen')
+            ->where('tb_driver.id_driver', $data['detail']->id_driver)
+            ->first();
+
+        $data['penumpang'] = DB::table('tb_detail_so')
+            ->select(
+                'tb_detail_so.id_detail_so',
+                'tb_detail_so.nama_penumpang',
+                'tb_detail_so.no_tlp',
+                'tb_detail_so.jabatan as nama_jabatan'
+            )
+            ->orderByDesc('id_detail_so')
+            ->where('id_service_order', $data['detail']->id_service_order)->get();
+
+        // return $data;
+        // return view('dashboard.export.exPdfPenugasan', $data);
+        $pdf = FacadePdf::loadView('dashboard.export.exPdfPenugasan', $data)->setPaper('f4', 'portrait');
+        set_time_limit(60);
+
+        return $pdf->download('laporan_penugasan_DO_' . $data['detail']->no_so . '.pdf');
+    }
+
+    public function exportPdfFilter(Request $request)
+    {
+        $tgl = $request->query('tgl_penjemputan');
+        $month = Carbon::parse($tgl)->format('m');
+        $year = Carbon::parse($tgl)->format('Y');
+        $data['bulan'] = Carbon::parse($tgl)->format('F Y');
+        $data['assignment'] = DB::table('tb_penugasan_driver')
+            ->select(
+                'tb_penugasan_driver.id_do',
+                'tb_order_kendaraan.no_so',
+                'tb_penugasan_driver.tgl_acc',
+                'tb_penugasan_driver.status_penugasan as status_do',
+                'tb_penugasan_driver.tgl_penugasan',
+                'tb_penugasan_driver.jam_berangkat',
+                'tb_driver.nama_driver',
+                'tb_petugas.nama_lengkap as nama_petugas',
+                'tb_kendaraan.nama_kendaraan',
+                'tb_kendaraan.no_polisi'
+            )
+            ->leftJoin('tb_order_kendaraan', 'tb_order_kendaraan.id_service_order', '=', 'tb_penugasan_driver.id_service_order')
+            ->leftJoin('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->leftJoin('tb_petugas', 'tb_petugas.id_petugas', '=', 'tb_penugasan_driver.id_petugas')
+            ->leftJoin('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->orderByDesc('id_do')
+            ->whereMonth('tb_penugasan_driver.tgl_penugasan', $month)
+            ->whereYear('tb_penugasan_driver.tgl_penugasan', $year)
+            ->get();
+        if ($data['assignment']->count() > 0) {
+
+            // return view('dashboard.export.exPdfPenugasanFilter', $data);
+            // return $data;
+            $pdf = FacadePdf::loadView('dashboard.export.exPdfPenugasanFilter', $data)->setPaper('f4', 'portrait');
+            set_time_limit(60);
+
+            return $pdf->download('laporan_penugasan_bulan_' . $tgl . '.pdf');
+        } else {
+            return redirect()->back()->with('success', 'Maaf, Bulan penugasan tidak ditemukan');
         }
     }
 }
