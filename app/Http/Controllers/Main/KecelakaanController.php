3be8f62d66adddee58d32ef19c26a02e1b4aa869
LuthfiRobit <luthfirobit@gmail.com>
2022-06-16 23:53:57 +0700

    add export pdf function

--- app/Http/Controllers/Main/KecelakaanController.php
+++ app/Http/Controllers/Main/KecelakaanController.php
@@ -9,6 +9,8 @@ use App\Models\Kecelakaan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
+use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
+use Carbon\Carbon;
 
 class KecelakaanController extends Controller
 {
@@ -85,7 +87,6 @@ class KecelakaanController extends Controller
         return view('dashboard.main.accident.detail', $data);
     }
 
-
     public function exportAcdFilter(Request $request)
     {
         $tgl_kecelakaan = $request->tgl_kecelakaan;
@@ -106,6 +107,86 @@ class KecelakaanController extends Controller
             return Excel::download(new KecelakaanExportOne($id), 'Laporan_kecelakaan_ACD' . $find->id_kecelakaan . '.xlsx');
         } else {
             return redirect()->back()->with('success', 'Maaf, Data kecelakaan tidak ditemukan');
+        }
+    }
+
+    public function exportPdfOne($id)
+    {
+        $data['kecelakaan'] = DB::table('tb_kecelakaan')
+            ->select(
+                'tb_kecelakaan.id_kecelakaan',
+                'tb_kecelakaan.id_do',
+                'tb_order_kendaraan.no_so',
+                'tb_order_kendaraan.tujuan',
+                'tb_kecelakaan.tgl_kecelakaan as tgl',
+                'tb_kecelakaan.jam_kecelakaan as jam',
+                'tb_kecelakaan.lokasi_kejadian as lokasi',
+                'tb_kecelakaan.kronologi',
+                'tb_kendaraan.kode_asset',
+                'tb_kendaraan.nama_kendaraan as kendaraan',
+                'tb_kendaraan.no_polisi',
+                'tb_kendaraan.warna',
+                'tb_kendaraan.jenis_penggerak',
+                'tb_merk_kendaraan.nama_merk as merk',
+                'tb_jenis_kendaraan.nama_jenis as jenis',
+                'tb_bahan_bakar.nama_bahan_bakar as bahan_bakar',
+                'tb_driver.nama_driver',
+                'tb_petugas.nama_lengkap as atasan',
+                'tb_detail_so.nama_penumpang as saksi'
+            )
+            ->leftJoin('tb_penugasan_driver', 'tb_penugasan_driver.id_do', '=', 'tb_kecelakaan.id_do')
+            ->leftJoin('tb_order_kendaraan', 'tb_order_kendaraan.id_service_order', '=', 'tb_penugasan_driver.id_service_order')
+            ->leftJoin('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->leftJoin('tb_bahan_bakar', 'tb_bahan_bakar.id_bahan_bakar', '=', 'tb_kendaraan.id_bahan_bakar')
+            ->leftJoin('tb_merk_kendaraan', 'tb_merk_kendaraan.id_merk', '=', 'tb_kendaraan.id_merk')
+            ->leftJoin('tb_jenis_kendaraan', 'tb_jenis_kendaraan.id_jenis_kendaraan', '=', 'tb_kendaraan.id_jenis_kendaraan')
+            ->leftJoin('tb_driver', 'tb_driver.id_driver', '=', 'tb_penugasan_driver.id_driver')
+            ->leftJoin('tb_saksi_kecelakaan', 'tb_saksi_kecelakaan.id_kecelakaan', '=', 'tb_kecelakaan.id_kecelakaan')
+            ->leftJoin('tb_petugas', 'tb_petugas.id_petugas', '=', 'tb_saksi_kecelakaan.id_atasan')
+            ->leftJoin('tb_detail_so', 'tb_detail_so.id_detail_so', '=', 'tb_saksi_kecelakaan.id_saksi')
+            ->where('tb_kecelakaan.id_kecelakaan', $id)
+            ->first();
+        if ($data['kecelakaan']) {
+            $pdf = FacadePdf::loadView('dashboard.export.exPdfKecelakaanOne', $data)->setPaper('f4', 'portrait');
+            set_time_limit(60);
+            return $pdf->download('laporan_kecalakaan_acd_' . $data['kecelakaan']->id_kecelakaan . '.pdf');
+        } else {
+            return redirect()->back()->with('success', 'Maaf, Data tidak ditemukan');
+        }
+    }
+
+    public function exportPdfFilter(Request $request)
+    {
+        $tgl = $request->query('tgl_kecelakaan');
+        $month = Carbon::parse($tgl)->format('m');
+        $year = Carbon::parse($tgl)->format('Y');
+        $data['filter'] = [
+            'bulan' => $tgl
+        ];
+        $data['kecelakaan'] = DB::table('tb_kecelakaan')
+            ->select(
+                'tb_kecelakaan.id_kecelakaan',
+                'tb_kecelakaan.id_do',
+                'tb_order_kendaraan.no_so',
+                'tb_kendaraan.nama_kendaraan as kendaraan',
+                'tb_kendaraan.no_polisi',
+                'tb_kecelakaan.tgl_kecelakaan as tgl',
+                'tb_kecelakaan.jam_kecelakaan as jam',
+                'tb_kecelakaan.lokasi_kejadian as lokasi',
+            )
+            ->leftJoin('tb_penugasan_driver', 'tb_penugasan_driver.id_do', '=', 'tb_kecelakaan.id_do')
+            ->leftJoin('tb_order_kendaraan', 'tb_order_kendaraan.id_service_order', '=', 'tb_penugasan_driver.id_service_order')
+            ->leftJoin('tb_kendaraan', 'tb_kendaraan.id_kendaraan', '=', 'tb_penugasan_driver.id_kendaraan')
+            ->whereMonth('tb_kecelakaan.tgl_kecelakaan', $month)
+            ->whereYear('tb_kecelakaan.tgl_kecelakaan', $year)
+            ->orderBy('tb_kecelakaan.tgl_kecelakaan')
+            ->get();
+        if ($data['kecelakaan']->count() > 0) {
+            $pdf = FacadePdf::loadView('dashboard.export.exPdfKecelakaanFilter', $data)->setPaper('f4', 'portrait');
+            set_time_limit(60);
+            return $pdf->download('laporan_kecelakaan_bulan_' . $tgl . '.pdf');
+        } else {
+            return redirect()->back()->with('success', 'Maaf, Data tidak ditemukan');
         }
     }
 }
