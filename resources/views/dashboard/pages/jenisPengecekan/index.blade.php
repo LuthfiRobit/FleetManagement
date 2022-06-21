a3ebd7d6a693a2c3b87b82ffe2234a646e014110
LuthfiRobit <luthfirobit@gmail.com>
2022-06-21 10:04:19 +0700

    add document ready in datatable master data

--- resources/views/dashboard/pages/jenisPengecekan/index.blade.php
+++ resources/views/dashboard/pages/jenisPengecekan/index.blade.php
@@ -213,6 +213,25 @@
 --}}
 <!--end::Page Custom Javascript-->
 <script text="text/javascipt">
+    $(document).ready(function () {
+        $("#kt_datatable_example_5").DataTable({
+            "language": {
+                "lengthMenu": "Show _MENU_",
+            },
+            "dom":
+                "<'row'" +
+                "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+                "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+                ">" +
+
+                "<'table-responsive'tr>" +
+
+                "<'row'" +
+                "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+                "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+                ">"
+        });
+    });
     "use strict";
     var KTModalNewTarget = function () {
         var t, e, n, a, o, i;
