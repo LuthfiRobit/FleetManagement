6a201a4e13ca58816f7336a36bc77cef99a2df3e
LuthfiRobit <luthfirobit@gmail.com>
2022-06-21 09:54:38 +0700

    add document function

--- resources/views/dashboard/main/assignmentbatal/index.blade.php
+++ resources/views/dashboard/main/assignmentbatal/index.blade.php
@@ -114,22 +114,25 @@
 <script src="{{ url('assets/backend/assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
 <!--end::Page Vendors Javascript-->
 <script text="text/javascipt">
-    $("#kt_datatable_do").DataTable({
-        "language": {
-            "lengthMenu": "Show _MENU_",
-        },
-        "dom":
-            "<'row'" +
-            "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
-            "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
-            ">" +
 
-            "<'table-responsive'tr>" +
+    $(document).ready(function () {
+        $("#kt_datatable_do").DataTable({
+            "language": {
+                "lengthMenu": "Show _MENU_",
+            },
+            "dom":
+                "<'row'" +
+                "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+                "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+                ">" +
 
-            "<'row'" +
-            "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
-            "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
-            ">"
+                "<'table-responsive'tr>" +
+
+                "<'row'" +
+                "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+                "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+                ">"
+        });
     });
 </script>
 @endpush
